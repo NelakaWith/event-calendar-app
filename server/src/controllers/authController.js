0ac4d6bc2fa58552eb35af6251b30506dce213b3
NelakaWith <nelaka.with@gmail.com>
2025-05-24 19:19:51 +0530

    feat: enhance registration and login responses with success messages

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -15,7 +15,10 @@ export const register = async (req, res) => {
     }
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({ email, name, password: hash });
-    res.status(201).json({ id: user.id, email: user.email, name: user.name });
+    res.status(201).json({
+      message: "Registration successful",
+      user: { id: user.id, email: user.email, name: user.name },
+    });
   } catch (err) {
     res
       .status(500)
@@ -42,11 +45,11 @@ export const login = async (req, res) => {
     res.cookie("access-token", token, {
       httpOnly: true,
       //   secure: process.env.NODE_ENV === "production",
-      //   sameSite: "lax",
+      sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
     res.json({
-      token,
+      message: "Login successful",
       user: { id: user.id, email: user.email, name: user.name },
     });
   } catch (err) {
