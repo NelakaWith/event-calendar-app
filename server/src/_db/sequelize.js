9a995b7c57d73e371bbadf06693805b492b11dd3
NelakaWith <nelaka.with@gmail.com>
2025-06-23 13:31:10 +0530

    refactor: restructure Sequelize initialization for better readability and maintainability

--- server/src/_db/sequelize.js
+++ server/src/_db/sequelize.js
@@ -1,16 +1,22 @@
-import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import { Sequelize } from "sequelize";
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false,
-  }
-);
+const dialect = process.env.DB_DIALECT || "sqlite";
+const storage = process.env.DB_STORAGE || ":memory:";
+
+const sequelize =
+  dialect === "sqlite"
+    ? new Sequelize({ dialect, storage, logging: false })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+          host: process.env.DB_HOST,
+          dialect,
+          logging: false,
+        }
+      );
 
 export default sequelize;
