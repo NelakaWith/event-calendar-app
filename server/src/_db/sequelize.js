e406013c7123de92a5c584fbaaa98410f9f6cbe9
NelakaWith <nelaka.with@gmail.com>
2025-06-23 13:32:41 +0530

    fix: change default database dialect from sqlite to mysql

--- server/src/_db/sequelize.js
+++ server/src/_db/sequelize.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 dotenv.config();
 
-const dialect = process.env.DB_DIALECT || "sqlite";
+const dialect = process.env.DB_DIALECT || "mysql";
 const storage = process.env.DB_STORAGE || ":memory:";
 
 const sequelize =
