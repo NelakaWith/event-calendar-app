7509070a17cbd435a63db93c4a2bdb4fb7539704
NelakaWith <nelaka.with@gmail.com>
2025-06-23 13:50:10 +0530

    fix: update CI workflow to use MySQL service and adjust Sequelize configuration

--- server/src/_db/sequelize.js
+++ server/src/_db/sequelize.js
@@ -2,13 +2,6 @@ import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 dotenv.config();
 
-console.log("[Sequelize] DB_DIALECT:", process.env.DB_DIALECT);
-console.log("[Sequelize] DB_STORAGE:", process.env.DB_STORAGE);
-console.log("[Sequelize] DB_NAME:", process.env.DB_NAME);
-console.log("[Sequelize] DB_USER:", process.env.DB_USER);
-console.log("[Sequelize] DB_PASS:", process.env.DB_PASS);
-console.log("[Sequelize] DB_HOST:", process.env.DB_HOST);
-
 const dialect = process.env.DB_DIALECT || "mysql";
 const storage = process.env.DB_STORAGE || ":memory:";
 
